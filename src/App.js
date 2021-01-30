4cf4634e031e080eb8af6b60ff01529c88e55c48
lukeown <lukeown@gmail.com>
2021-01-30 15:45:54 -0500

    revert back to class based state management to follow course project

--- src/App.js
+++ src/App.js
@@ -1,44 +1,45 @@
-import React, { useState } from 'react'; //useState is hook that lets us manage state in a function component
+import React, { Component } from 'react';
 import './App.css';
-import Task from './Task/Task'; //imports Task component from Task.js
+import Task from './Task/Task';
 
-const app = props => {
-    const [ tasksState, setTasksState ] = useState({
+class App extends Component {
+  state = {
+    tasks: [
+      {taskName: 'Do Dishes', duration: '30', importance: '!'},
+      {taskName: 'Clean Desk', duration: '10', importance: '!'},
+      {taskName: 'Make Dinner', duration: '50', importance: '!!'},
+      {taskName: 'Finish To Do List Project', duration: '120', importance: '!!!'}
+    ],
+    otherState: 'some other value'
+  };
+
+  switchTaskHandler = () => {
+    // console.log('Was clicked!');
+    // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
+    this.setState({
       tasks: [
-        {taskName: 'Do Dishes', duration: '30', importance: '!'},
-        {taskName: 'Clean Desk', duration: '10', importance: '!'},
-        {taskName: 'Make Dinner', duration: '50', importance: '!!'},
-        {taskName: 'Finish To Do List Project', duration: '120', importance: '!!!'}
-      ],
+        {taskName: 'Do Laundry', duration: 'passive', importance: '!'},
+        {taskName: 'Take Out Trash', duration: '5', importance: '!!!'},
+        {taskName: 'Clean out fridge', duration: '20', importance: '!!'},
+        {taskName: 'Finish To Do List Project (thought you were gonna get out of this one?)', duration: 'whatever it takes, do not give up.', importance: '!!!!'}
+      ] 
     });
-     // uses multiple useState calls with different state slices
-    const [otherState, setOtherState] = useState('some other value')
+  };
 
-    console.log(tasksState, otherState)
-
-    const switchTaskHandler = () => {
-      setTasksState({ //This passes the following tasks when the 'Switch Task' button is clicked
-        tasks: [
-          {taskName: 'Do Laundry', duration: 'passive', importance: '!'},
-          {taskName: 'Take Out Trash', duration: '5', importance: '!!!'},
-          {taskName: 'Clean out fridge', duration: '20', importance: '!!'},
-          {taskName: 'Finish To Do List Project (thought you were gonna get out of this one?)', duration: 'whatever it takes, do not give up.', importance: '!!!!'}
-        ] 
-      });
-    }
+  render() {
     return (
       <div className="App">
-        <h1>Tasks:</h1>
-        <button onClick={switchTaskHandler}>Switch Tasks</button>
-        <Task taskName={tasksState.tasks[0].taskName} duration={tasksState.tasks[0].duration} importance={tasksState.tasks[0].importance}/>
-        <Task taskName={tasksState.tasks[1].taskName} duration={tasksState.tasks[1].duration} importance={tasksState.tasks[1].importance}/>
-        <Task taskName={tasksState.tasks[2].taskName} duration={tasksState.tasks[2].duration} importance={tasksState.tasks[2].importance}/>
-        <Task taskName={tasksState.tasks[3].taskName} duration={tasksState.tasks[3].duration} importance={tasksState.tasks[3].importance}/>
-      </div> // Person is calling the Person function in person.js. When inspected, just the HTML that's exported is displayed
-    ); //button refers to const switchTaskHandler function
-    
+        <h1>To Do List</h1>
+        <p>Click Switch Task button for new tasks</p>
+        <button onClick={this.switchTaskHandler}>Switch Task</button>
+        <Task taskName={this.state.tasks[0].taskName} duration={this.state.tasks[0].duration} importance={this.state.tasks[0].importance}/>
+        <Task taskName={this.state.tasks[1].taskName} duration={this.state.tasks[1].duration} importance={this.state.tasks[1].importance}/>
+        <Task taskName={this.state.tasks[2].taskName} duration={this.state.tasks[2].duration} importance={this.state.tasks[2].importance}/>
+        <Task taskName={this.state.tasks[3].taskName} duration={this.state.tasks[3].duration} importance={this.state.tasks[3].importance}/>
+      </div>
+    );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
+  }
 }
 
-
-
-export default app;
+export default App;
