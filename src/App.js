c23eece286a45a026ce8dcb711faf5abc0179967
lukeown <lukeown@gmail.com>
2021-01-30 14:59:27 -0500

    add comments to better understand app; add new content to state handler

--- src/App.js
+++ src/App.js
@@ -12,27 +12,26 @@ const app = props => {
       ],
     });
 
-    const switchNameHandler = () => {
-      //console.log('clicked!!!');
-      setTasksState({
+    const switchTaskHandler = () => {
+      setTasksState({ //This passes the following tasks when the 'Switch Task' button is clicked
         tasks: [
-          {taskName: 'Do Dishes', duration: '40', importance: '!!'},
-          {taskName: 'Clean Desk', duration: '20', importance: '!!'},
-          {taskName: 'Make Dinner', duration: '60', importance: '!!!'},
-          {taskName: 'Finish To Do List Project', duration: '120', importance: '!'}
+          {taskName: 'Do Laundry', duration: 'passive', importance: '!'},
+          {taskName: 'Take Out Trash', duration: '5', importance: '!!!'},
+          {taskName: 'Clean out fridge', duration: '20', importance: '!!'},
+          {taskName: 'Finish To Do List Project (thought you were gonna get out of this one?)', duration: 'whatever it takes, do not give up.', importance: '!!!!'}
         ] 
       });
     }
     return (
       <div className="App">
         <h1>Tasks:</h1>
-        <button onClick={switchNameHandler}>Switch Task</button>
+        <button onClick={switchTaskHandler}>Switch Tasks</button>
         <Task taskName={tasksState.tasks[0].taskName} duration={tasksState.tasks[0].duration} importance={tasksState.tasks[0].importance}/>
         <Task taskName={tasksState.tasks[1].taskName} duration={tasksState.tasks[1].duration} importance={tasksState.tasks[1].importance}/>
         <Task taskName={tasksState.tasks[2].taskName} duration={tasksState.tasks[2].duration} importance={tasksState.tasks[2].importance}/>
         <Task taskName={tasksState.tasks[3].taskName} duration={tasksState.tasks[3].duration} importance={tasksState.tasks[3].importance}/>
       </div> // Person is calling the Person function in person.js. When inspected, just the HTML that's exported is displayed
-    );
+    ); //button refers to const switchTaskHandler function
     
 }
 
